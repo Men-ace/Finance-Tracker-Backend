a3fbc47186a55552ac09a17b74a829b57a35cddd
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-04-14 17:29:13 +1000

    ff

--- lms-premsir/lms_fe/src/components/forms/index.js
+++
@@ -1,2 +0,0 @@
-export { default as NewBookForm } from "./bookFroms/NewBookForm";
-export { default as EditBookForm } from "./bookFroms/EditBookForm";
--- lms-premsir/lms_fe/src/components/layouts/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-  return (
-    <div className="bg-dark text-white p-4 text-center">
-      &copy; Copy right all reserved! Made by Prem with Fun!
-    </div>
-  );
-};
